71289eb1e9a030fc70de03260170373068d191b1
udinesia325 <udinesia325@gmail.com>
2021-07-18 18:23:19 +0700

    menghilangkan konsol dan atur favico

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
-import eruda from "eruda"
-eruda.init()
 
 
 
 
 
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,document.getElementById('root'));
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
 
 
 
